c6acd7dbecc35641b5e337f6719d42436bfe6c0f
JamesClonk <jamesclonk@jamesclonk.ch>
2015-02-07 15:45:39 +0100

    create sshkey, delete sshkey

--- cmd/commands.go
+++ cmd/commands.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"log"
 	"runtime"
 
 	vultr "github.com/JamesClonk/vultr/lib"
@@ -11,11 +10,11 @@ import (
 
 func (c *CLI) RegisterCommands() {
 	// sshkeys
-	c.Command("sshkey", "control SSH public keys on Vultr account", func(cmd *cli.Cmd) {
-		cmd.Command("create", "upload and add new SSH public key to your Vultr account", printAPIKey)
+	c.Command("sshkey", "control SSH public keys on your Vultr account", func(cmd *cli.Cmd) {
+		cmd.Command("create", "upload and add new SSH public key to your Vultr account", sshKeysCreate)
 		cmd.Command("update", "update an existing SSH public key in your Vultr account", sshKeysUpdate)
-		cmd.Command("delete", "remove an existing SSH public key from your Vultr account", printAPIKey)
-		cmd.Command("list", "list all SSH public keys in Vultr account", sshKeysList)
+		cmd.Command("delete", "remove an existing SSH public key from your Vultr account", sshKeysDelete)
+		cmd.Command("list", "list all existing SSH public keys from your Vultr account", sshKeysList)
 	})
 
 	// version
@@ -34,50 +33,5 @@ func (c *CLI) RegisterCommands() {
 func printAPIKey(cmd *cli.Cmd) {
 	cmd.Action = func() {
 		fmt.Println(*apiKey)
-	}
-}
-
-func sshKeysUpdate(cmd *cli.Cmd) {
-	cmd.Spec = "SSHKEYID [-n | --name] [-k | --key]"
-
-	id := cmd.StringArg("SSHKEYID", "", "SSHKEYID of key to update (see <sshkey list>)")
-	name := cmd.StringOpt("n name", "", "New name for the SSH key")
-	key := cmd.StringOpt("k key", "", "New SSH key contents")
-
-	cmd.Action = func() {
-		sshkey := vultr.SSHKey{
-			ID:   *id,
-			Name: *name,
-			Key:  *key,
-		}
-		if err := GetClient().UpdateSSHKey(sshkey); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("SSH key update success!")
-	}
-}
-
-func sshKeysList(cmd *cli.Cmd) {
-	cmd.Spec = "[-f | --full]"
-
-	full := cmd.BoolOpt("f full", false, "Display full length of SSH key")
-
-	cmd.Action = func() {
-		keys, err := GetClient().GetSSHKeys()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		keyLength := 64
-		if *full {
-			keyLength = 8192
-		}
-		lengths := []int{24, 32, keyLength}
-
-		printTabbedLine([]string{"SSHKEYID", "NAME", "KEY"}, lengths)
-		for _, key := range keys {
-			printTabbedLine([]string{key.ID, key.Name, key.Key}, lengths)
-		}
-		tabsFlush()
 	}
 }
---
+++ cmd/commands_sshkeys.go
@@ -0,0 +1,85 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	vultr "github.com/JamesClonk/vultr/lib"
+	"github.com/jawher/mow.cli"
+)
+
+func sshKeysCreate(cmd *cli.Cmd) {
+	cmd.Spec = "-n | --name -k | --key"
+
+	name := cmd.StringOpt("n name", "", "Name of the SSH key")
+	sshkey := cmd.StringOpt("k key", "", "SSH public key (in authorized_keys format)")
+
+	cmd.Action = func() {
+		key, err := GetClient().CreateSSHKey(*name, *sshkey)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("SSH key create success!\n")
+		lengths := []int{24, 32, 64}
+		printTabbedLine([]string{"SSHKEYID", "NAME", "KEY"}, lengths)
+		printTabbedLine([]string{key.ID, key.Name, key.Key}, lengths)
+		tabsFlush()
+	}
+}
+
+func sshKeysUpdate(cmd *cli.Cmd) {
+	cmd.Spec = "SSHKEYID [-n | --name] [-k | --key]"
+
+	id := cmd.StringArg("SSHKEYID", "", "SSHKEYID of key to update (see <sshkey list>)")
+	name := cmd.StringOpt("n name", "", "New name for the SSH key")
+	key := cmd.StringOpt("k key", "", "New SSH key contents")
+
+	cmd.Action = func() {
+		sshkey := vultr.SSHKey{
+			ID:   *id,
+			Name: *name,
+			Key:  *key,
+		}
+		if err := GetClient().UpdateSSHKey(sshkey); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("SSH key update success!")
+	}
+}
+
+func sshKeysDelete(cmd *cli.Cmd) {
+	id := cmd.StringArg("SSHKEYID", "", "SSHKEYID of key to delete (see <sshkey list>)")
+
+	cmd.Action = func() {
+		if err := GetClient().DeleteSSHKey(*id); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("SSH key delete success!")
+	}
+}
+
+func sshKeysList(cmd *cli.Cmd) {
+	cmd.Spec = "[-f | --full]"
+
+	full := cmd.BoolOpt("f full", false, "Display full length of SSH key")
+
+	cmd.Action = func() {
+		keys, err := GetClient().GetSSHKeys()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		keyLength := 64
+		if *full {
+			keyLength = 8192
+		}
+		lengths := []int{24, 32, keyLength}
+
+		printTabbedLine([]string{"SSHKEYID", "NAME", "KEY"}, lengths)
+		for _, key := range keys {
+			printTabbedLine([]string{key.ID, key.Name, key.Key}, lengths)
+		}
+		tabsFlush()
+	}
+}
